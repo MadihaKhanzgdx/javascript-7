54028d2645aa805f86e8d1d7c0fe4d82487dceb1
Madihakhanzgdx <madihakhan32727@gmail.com>
2025-09-26 07:12:43 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,108 @@
+// chapter :35--45
+
+
+
+// Question :1
+// var date = new Date;
+// document.write(date)
+
+
+// Question :2
+// var firstName;
+// var lastName;
+// var fullName
+// function greet(){
+// firstName =prompt("Enter first name")
+//  lastName =prompt("Enter last name");
+//  fullName = (firstName +lastName);
+
+// document.write("Good Morning" + " " +  fullName )
+// }
+// greet()
+
+
+// Question :3
+// function sum(num1, num2) {
+//   var add = num1 + num2;
+//   return add;
+// }
+// var use = sum(+prompt("Enter first number") , +prompt("Enter second number"));
+
+// console.log(use);
+
+
+// Question :4
+
+// Question :5
+// function square(num) {
+//   return num * num;
+// }
+
+// console.log(square(5));  
+
+
+// Question :6
+// function factorial(n) {
+//   if (n < 0) return undefined;
+//   if (n === 0 || n === 1) return 1;
+//   return n * factorial(n - 1);
+// }
+
+// console.log(factorial(+prompt("ENter a number")));
+
+
+// Question :7
+// function multiplication_table(table, range) {
+
+//     for (var i = 1; i <= range; i++) {
+//       document.write(table, "x", i, "=", table * i, "<br>");
+//     }
+//   }
+//   multiplication_table(
+//     +prompt("Enter a number"),
+//      +prompt("Enter a range"));
+
+
+// Question :8
+
+// // Question :9
+// function area(width,height){
+// var areaoftriangle =(width*height);
+// console.log(areaoftriangle);
+// }
+
+// area("5","5")
+
+
+// Question :10
+var word = prompt("Enter a word");
+
+var len = word.length;
+
+var reverse = "";
+
+for (var i = len - 1; i >= 0; i--) {
+  reverse = reverse + word[i];
+}
+
+if (reverse === word) {
+  console.log("its a palindrome word");
+} else {
+  console.log("its not a palindrome word");
+}
+// Question :11
+// Question :12
+// Question :13
+// Question :14
+// Question :15
+// Question :16
+// Question :17
+// Question :18
+// Question :19
+// Question :20
+// Question :21
+// Question :22
+// Question :23
+// Question :24
+// Question :25
+// Question :26
